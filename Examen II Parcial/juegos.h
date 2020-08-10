502e90b0fc9d295b3d416b844f2a648db820bcce
CalyCastillo <68139144+CalyCastillo@users.noreply.github.com>
2020-08-09 23:04:29 -0600

    Add files via upload

---
+++ Examen II Parcial/juegos.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <stdlib.h>
+#include<time.h>
+
+void inicializarArreglo();
+void dificultad();
+void gotoxy(int x, int y);
+void pintar();
+bool gameover();
+void proceso();
+
+
+
